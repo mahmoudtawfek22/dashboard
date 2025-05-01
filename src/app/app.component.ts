6a17afc0995b92ab7892f36372565dc736618c46
mahmoudtawfek22 <momohoho2224@gmail.com>
2025-05-01 18:08:54 +0300

    improve dashboard responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +11,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'dashboard';
+  constructor(private router: Router) {
+    if (localStorage.getItem('token')) {
+      this.router.navigate(['dashboard']);
+    }
+  }
 }
